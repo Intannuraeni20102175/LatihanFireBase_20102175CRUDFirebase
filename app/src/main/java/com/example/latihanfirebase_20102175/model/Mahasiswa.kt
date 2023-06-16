be84e074b2a2f1c84ff6c2995aa8658c4134f911
Intannuraeni20102175 <20102175@ittelkom-pwt.ac.id>
2023-06-16 12:43:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanfirebase_20102175/model/Mahasiswa.kt
@@ -0,0 +1,8 @@
+package com.example.latihanfirebase_20102175.model
+
+data class Mahasiswa(
+    val id : String =" ",
+    val nim : String = " ",
+    val nama : String = " ",
+    val telepon : String = " "
+)
---
+++ app/src/main/java/com/example/latihanfirebase_20102175/view/HomeFragment.kt
@@ -0,0 +1,59 @@
+package com.example.latihanfirebase_20102175.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.latihanfirebase_20102175.R
+import com.example.latihanfirebase_20102175.adapter.HomeAdapter
+import com.example.latihanfirebase_20102175.databinding.FragmentHomeBinding
+import com.example.latihanfirebase_20102175.model.Mahasiswa
+import com.google.firebase.database.*
+
+class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
+    lateinit var adapter : HomeAdapter
+    lateinit var databaseRef : DatabaseReference
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = HomeAdapter(ArrayList())
+        binding.rvUser.adapter = adapter
+        binding.rvUser.layoutManager = LinearLayoutManager(requireContext())
+
+        databaseRef = FirebaseDatabase.getInstance().getReference("mahasiswa")
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val mahasiswaList = ArrayList<Mahasiswa>()
+                for (snapshot in dataSnapshot.children) {
+                    val mahasiswa = snapshot.getValue(Mahasiswa::class.java)
+                    mahasiswa?.let {
+                        mahasiswaList.add(it)
+                    }
+                }
+                adapter.setData(mahasiswaList)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //Handler Error
+            }
+
+        })
+        binding.btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_tambahFragment)
+        }
+    }
+}
